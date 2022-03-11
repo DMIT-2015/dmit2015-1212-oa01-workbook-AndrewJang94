d38966f58212149140064ca876a5ff4a4fc1510d
Yun Jang <dbsgur45@gmail.com>
2022-03-11 13:18:42 -0700

    deleted assignment

--- dmit2015-assignment04-yjang/src/main/java/ca/nait/dmit/dmit2015/yjang/assignment04/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package ca.nait.dmit.dmit2015.yjang.assignment04;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/webapi")
-public class HelloApplication extends Application {
-
-}
